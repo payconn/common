04e9f135bf2b7eeda4028fe9089d2572d377dd7b
Murat SAÇ <muratsac@mail.com>
2019-05-12 22:36:30 +0300

    gateway interface

--- src/Common/GatewayInterface.php
+++ src/Common/GatewayInterface.php
@@ -5,4 +5,16 @@ namespace Payconn\Common;
 interface GatewayInterface
 {
     public function createRequest(string $class, array $parameters);
+
+    public function authorize(array $parameters);
+
+    public function authorizeComplete(array $parameters);
+
+    public function purchase(array $parameters);
+
+    public function purchaseComplete(array $parameters);
+
+    public function void(array $parameters);
+
+    public function refund(array $parameters);
 }
